0c12d34c119d518377d97ae64b7a2a93625f6a48
CastilloRodrig0 <rcastillo288@gmail.com>
2021-09-19 21:13:23 -0300

    .

---
+++ JS/login.js
@@ -0,0 +1,22 @@
+function ingresar(){
+let nombreUsuario=document.getElementById('user');
+let contraseña=document.getElementById('pass');
+
+}
+
+let variable =4;
+console.log(variable);
+{
+    let variable =3;
+    console.log(variable);
+}
+variable +=2;
+console.log(variable);
+
+let texto = 'texto';
+console.log(texto.toUpperCase());
+
+if (0 == '0'){
+    console.log(texto.toUpperCase());
+
+}
---
+++ JS/principal.js
@@ -0,0 +1,11 @@
+class Card{
+constructor (imagen,titulo){
+    this.image=imagen;
+    this.tit=titulo;
+}
+}
+
+function crearCard(){
+    let card = document.getElementById('cuerpo');
+    let     
+}
